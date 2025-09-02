f79bc46661d8c0c9196033093a880bee4c077ff1
Gajendra Sharma <gajendrasharma0145@gmail.com>
2025-09-02 17:59:39 +0530

    Deleted Profile Component

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import HomePage from "./pages/HomePage";
 import BusesPage from "./pages/BusesPage";
 import DriversPage from "./pages/DriversPage";
 import BusRoutesPage from "./pages/BusRoutesPage";
-import ProfilePage from "./pages/ProfilePage";
 import StudentsByRoute from "./components/Routes/StudentsByRoute/StudentsByRoute";
 
 function App() {
@@ -36,7 +35,6 @@ function App() {
           <Route path="buses" element={<BusesPage />} />
           <Route path="drivers" element={<DriversPage />} />
           <Route path="routes" element={<BusRoutesPage />} />
-          <Route path="profile" element={<ProfilePage />} />
           <Route
             path="routes/:routeId/students"
             element={<StudentsByRoute />}
--- src/pages/ProfilePage.tsx
+++
@@ -1,5 +0,0 @@
-import Profile from "../components/Profile/Profile";
-
-const ProfilePage = () => <Profile />;
-
-export default ProfilePage;
